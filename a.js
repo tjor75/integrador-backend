afe70f6ad7d935d4a3760e63779263f060f66a92
Lucas Tjor <tjor75@gmail.com>
2025-08-07 02:00:13 -0300

    Eliminar a.js

--- a.js
+++
@@ -1,7 +0,0 @@
-import * as eventService from "./src/services/event-service.js";
-
-try {
-    const badRequest = await eventService.unenrollAsync("a", "a");
-} catch (error) {
-    console.error("/" + typeof error, "/" + error.message, "/" + error.name);
-}
